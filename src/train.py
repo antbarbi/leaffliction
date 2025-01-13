2e8db669dbe8a6edbd9a2f366679280840dcbb2c
Antoine BARBIER <antbarbi@paul-f4Ar8s1.clusters.42paris.fr>
2025-01-13 10:21:38 +0100

    first step of adding training

---
+++ src/train.py
@@ -0,0 +1,175 @@
+import os
+import argparse
+import torch
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader, random_split
+from torchvision.io import read_image, ImageReadMode
+from torchvision import transforms
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ImageDataset(Dataset):
+    def __init__(self, root_dir: str, resize: tuple = (3, 64, 64)):
+        self.root_dir = root_dir
+        self.image_files = []
+        self.classes = sorted(entry.name for entry in os.scandir(root_dir) if entry.is_dir())
+        self.class_to_idx = {cls_name: idx for idx, cls_name in enumerate(self.classes)}
+        self.num_classes = len(self.classes)
+        self.resize = resize
+        
+        for root, dirs, files in os.walk(root_dir):
+            if not dirs:
+                for file in files:
+                    class_name = os.path.basename(root)
+                    self.image_files.append((file, class_name))
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, index):
+        name, label = self.image_files[index]
+        img_path = os.path.join(self.root_dir, label, name)
+        image = read_image(img_path, mode=ImageReadMode.RGB).float() / 255.0
+        image = transforms.Resize(size=(64, 64))(image)
+        label_idx = self.class_to_idx[label]
+        one_hot_label = F.one_hot(torch.tensor(label_idx), num_classes=self.num_classes)
+        return image, label_idx
+
+
+class CNN(nn.Module):
+    def __init__(self, num_of_classes: int, input_size: tuple):
+        super(CNN, self).__init__()
+        self.size = input_size
+
+        self.conv_layer1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        self.conv_layer2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        def _get_flattened_size(self):
+            with torch.no_grad():
+                dummy_input = torch.zeros(1, *self.size)
+                out = self.conv_layer1(dummy_input)
+                out = self.pool1(out)
+                out = self.conv_layer2(out)
+                out = self.pool2(out)
+                flattened_size = out.view(1, -1).size(1)
+            return flattened_size
+
+        self.fc1 = nn.Linear(_get_flattened_size(self), 128)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(128, num_of_classes)
+
+
+    def _get_flattened_size(self, input_size=(3, 64, 64)):
+        with torch.no_grad():
+            dummy_input = torch.zeros(1, *input_size)
+            out = self.conv_layer1(dummy_input)
+            out = self.pool1(out)
+            out = self.conv_layer2(out)
+            out = self.pool2(out)
+            flattened_size = out.view(1, -1).size(1)
+        return flattened_size
+
+
+    def forward(self, x):
+        out = self.conv_layer1(x)
+        out = self.pool1(out)
+
+        out = self.conv_layer2(out)
+        out = self.pool2(out)
+
+        out = out.reshape(out.size(0), -1)
+
+        out = self.fc1(out)
+        out = self.relu1(out)
+        out = self.fc2(out)
+        return out
+
+
+def args_parser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="Data augmentation",
+    )
+    parser.add_argument(
+        "src",
+        help="The src has to be a folder \
+            behavior differs depending on the type of the src",
+        type=str
+    )
+    return parser.parse_args()
+
+
+def main(src):
+    # 1 - Get data ready (turned into tensors)
+    dataset = ImageDataset(src)
+    dataset_size = len(dataset)
+    train_size = int(0.8 * dataset_size)
+    test_size = dataset_size - train_size
+ 
+    train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+    train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=12)
+    test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=True, num_workers=12)
+
+    img: torch.Tensor = dataset[0][0]
+
+    model = CNN(8, dataset.resize)
+    print(f"Flattened size for fc1: {model._get_flattened_size()}")
+    # exit()
+
+    # Hyperparams
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.0001, weight_decay=0.005, momentum=0.9)
+    total_step = len(train_dataloader)
+    patience = 10
+    best_loss = float('inf')
+    counter = 0
+
+    device = torch.device("cpu")
+    model.to(device)
+
+    for epoch in range(1000):
+        model.train()
+        for i, (images, labels) in enumerate(train_dataloader):
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if "val_loss" not in locals():
+                val_loss = 0
+            print(f"Epoch [{epoch+1}/{1000}], Step [{i+1}/{total_step}], Loss: {loss.item():.4f}, Val_loss: {val_loss:.4f}, Best-Loss: {best_loss:.4f}")
+           # Validation
+
+        model.eval()
+        val_loss = 0
+        with torch.no_grad():
+            for images, labels in test_dataloader:
+                images = images.to(device)
+                labels = labels.to(device)
+                outputs = model(images)
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()
+        val_loss /= len(test_dataloader)
+        print(f"Validation Loss: {val_loss:.4f}")
+
+        if val_loss < best_loss:
+            best_loss = val_loss
+            counter = 0
+        else:
+            counter += 1
+            if counter >= patience:
+                print("Early stopping")
+                break
+
+
+if __name__ == "__main__":
+    args = args_parser()
+    main(args.src)

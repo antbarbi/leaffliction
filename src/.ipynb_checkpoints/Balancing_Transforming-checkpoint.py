6574e7d5dc71fe3b6bbe239e76a5eeba7282a63f
Antoine Barbier <antbarbi@student.42.fr>
2025-01-13 15:35:12 +0000

    gpu training

---
+++ src/.ipynb_checkpoints/Balancing_Transforming-checkpoint.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import os
+import argparse
+import random
+import Augmentation
+import Transformation
+
+NUM_OF_AUGMENTATION = 6
+Augmentation.verbose = False
+Transformation.verbose = False
+
+
+def args_parser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="Data augmentation",
+    )
+    parser.add_argument(
+        "directory",
+        help="The name of the folder where to do apply balancing",
+        type=str
+    )
+    return parser.parse_args()
+
+
+def main(directory: str):
+    datasets = {}
+    for root, dirs, files in os.walk(directory):
+        if not dirs:
+            datasets[len(files)] = (root, files)
+
+    sorted_key = sorted(datasets.keys())
+    MAX = sorted_key[-1]
+    datasets = {key: datasets[key] for key in sorted_key[:-1]}
+
+    for length, (path, filenames) in datasets.items():
+        random.shuffle(filenames)
+        for index, filename in enumerate(filenames):
+            if length + index * NUM_OF_AUGMENTATION >= MAX:
+                break
+            pathname = os.path.join(path, filename)
+            Augmentation.main(pathname)
+    print("Datasets have been augmented and balanced.")
+
+    for root, dirs, files in os.walk(directory):
+        for dir in dirs:
+            directory = os.path.join(root, dir)
+            Transformation.main(src=directory, dst=directory)
+    print("Datasets have been transformed.")
+
+
+if __name__ == "__main__":
+    args = args_parser()
+    main(args.directory)

1613f049fb5003d746bc0863aea25cfd6097b64f
Antoine BARBIER <antbarbi@paul-f4Ar8s2.clusters.42paris.fr>
2025-01-15 17:01:05 +0100

    added propem args for predict

--- src/predict.py
+++ src/predict.py
@@ -2,6 +2,8 @@ import torch
 import argparse
 import matplotlib.pyplot as plt
 from torch_classes import ImageDataset, CNN
+from torchvision.io import read_image, ImageReadMode
+from torchvision import transforms
 
 
 NUM_OF_CLASSES = 8
@@ -11,18 +13,21 @@ def args_parser() -> argparse.Namespace:
         prog="Data augmentation",
     )
     parser.add_argument(
-        "src",
+        "-s",
+        "--source",
         help="The src has to be a folder \
             behavior differs depending on the type of the src",
         type=str
     )
     parser.add_argument(
-        "img",
+        "-i",
+        "--image",
         help="The image to have to prediction on",
         type=str
     )
     parser.add_argument(
-        "weight",
+        "-w",
+        "--weight",
         help="The image to have to prediction on",
         type=str
     )
@@ -30,16 +35,14 @@ def args_parser() -> argparse.Namespace:
 
 
 def main(src: str, image_pth: str, weights_pth: str, map_location="cpu"):
-    print("Loading dataset")
     dataset = ImageDataset(src)
-    print("Dataset is loaded")
+    print("Dataset is loaded for class labeling")
 
     model = CNN(NUM_OF_CLASSES, dataset.resize)
     model.load_state_dict(torch.load(weights_pth, map_location, weights_only=True))
     model.eval()
 
-    img: torch.Tensor = dataset[INDEX][0]
-    print(img.shape, dataset[INDEX][1], dataset.classes[dataset[INDEX][1]])
+    img: torch.Tensor = read_image(image_pth, mode=ImageReadMode.RGB).float() / 255.0
 
     with torch.no_grad():
         input_tensor = img.unsqueeze(0)
@@ -52,4 +55,4 @@ def main(src: str, image_pth: str, weights_pth: str, map_location="cpu"):
 if __name__ == "__main__":
     args = args_parser()
     print(args)
-    main(args.src, args.img, args.weight)
+    main(args.source, args.image, args.weight)
